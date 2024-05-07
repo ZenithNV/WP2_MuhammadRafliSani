f7315090b6ddbb4f3cf01709fd1c65088dc78972
Snr.xt <101034784+ZenithNV@users.noreply.github.com>
2024-05-07 09:58:03 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+{
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya MUHAMMAD RAFLI SANI Saya tinggal di Pamulang Saya suka musik";
+}
+}
